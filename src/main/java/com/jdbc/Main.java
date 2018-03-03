a6be20a31022fe14efca580f426da5527b0689b1
veselovoleg <veselovoleg.nn@gmail.com>
2018-03-03 16:37:34 +0300

    Final view

--- src/main/java/com/jdbc/Main.java
+++ src/main/java/com/jdbc/Main.java
@@ -12,17 +12,26 @@ public class Main {
         DBProcessor db = new DBProcessor();
         Connection conn = db.getConnection(URL, USERNAME, PASSWORD);
 
-        String query = "select * from business.products"; //Print only this choose
+
+        //We can insert our data to the table
+        String INSERT = "insert into business.products (productname, price, shop_id) values (?,?,?)";
+        PreparedStatement preparedStatementInsert = conn.prepareStatement(INSERT);
+        preparedStatementInsert.setString(1,"Naggets");
+        preparedStatementInsert.setDouble(2, 98.35);
+        preparedStatementInsert.setInt(3, 2);
+        preparedStatementInsert.execute();
 
         //The query is written to the memory allocated to the object preparedStatement.
         // Next, we can use it many times in a row at different values.
-        PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ResultSet resultSet = preparedStatement.executeQuery();
+        String query = "select * from business.products";
+        PreparedStatement preparedStatementForSelect = conn.prepareStatement(query);
+        ResultSet resultSet = preparedStatementForSelect.executeQuery();
 
         while (resultSet.next()) {
-            System.out.println(resultSet.getString("productname"));
+            System.out.println(resultSet.getInt("product_id")+ " "+resultSet.getString("productname"));
         }
 
+        preparedStatementForSelect.close();
         conn.close();
 
 
