65c541e27aae19b503421bc7b1fed0ec30df4a89
veselovoleg <veselovoleg.nn@gmail.com>
2018-03-03 15:57:54 +0300

    Lesson 6 - PreparedStatement

--- src/main/java/com/jdbc/Main.java
+++ src/main/java/com/jdbc/Main.java
@@ -1,9 +1,6 @@
 package com.jdbc;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     private static final String USERNAME = "root";
@@ -11,29 +8,21 @@ public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
 
     public static void main(String[] args) throws SQLException {
+
         DBProcessor db = new DBProcessor();
         Connection conn = db.getConnection(URL, USERNAME, PASSWORD);
-        //String query = "select * from business.products where product_id = 6"; //Print only this choose
-        String query = "select * from business.products";
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
 
-        while (resultSet.next()) { //return True, if you have next element
-            int id;
-            String name;
-            double price;
-            int shopID;
+        String query = "select * from business.products"; //Print only this choose
 
-            id = resultSet.getInt("product_id");
-            name = resultSet.getString("productname");
-            price = resultSet.getDouble("price");
-            shopID = resultSet.getInt("shop_id");
-            //Always close connection
-            Product product = new Product(id, name, price, shopID);
-            System.out.println(product);
+        //The query is written to the memory allocated to the object preparedStatement.
+        // Next, we can use it many times in a row at different values.
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("productname"));
         }
 
-        statement.close();
         conn.close();
 
 
